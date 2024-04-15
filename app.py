9abcc4d084a803ce7c8a6bb40dd3c251f3290a3e
Fathin Afif <fathinafiff@gmail.com>
2024-04-15 12:55:40 +0700

    shutdown api server

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 from flask import Flask, request, render_template
-from flask_sqlalchemy import SQLAlchemy
+
+# from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = (
-    "postgresql://username:password@localhost/database_name"
-)
-db = SQLAlchemy(app)
+# app.config["SQLALCHEMY_DATABASE_URI"] = (
+#     "postgresql://username:password@localhost/database_name"
+# )
+# db = SQLAlchemy(app)
 
 
-class Email(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+# class Email(db.Model):
+# id = db.Column(db.Integer, primary_key=True)
+# email = db.Column(db.String(120), unique=True, nullable=False)
 
 
 @app.route("/")
@@ -19,14 +20,14 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/add_email", methods=["POST"])
-def add_email():
-    if request.method == "POST":
-        email = request.form["email"]
-        new_email = Email(email=email)
-        db.session.add(new_email)
-        db.session.commit()
-        return "Email added successfully!"
+# @app.route("/add_email", methods=["POST"])
+# def add_email():
+#     if request.method == "POST":
+#         email = request.form["email"]
+#         new_email = Email(email=email)
+#         db.session.add(new_email)
+#         db.session.commit()
+#         return "Email added successfully!"
 
 
 if __name__ == "__main__":
